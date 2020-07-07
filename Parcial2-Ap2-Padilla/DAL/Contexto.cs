8150f535af7a9c9cdd8c9e5ac3900e99d42da492
Estiven Padilla <estivenps26@gmail.com>
2020-07-07 10:10:31 -0400

    Preparando repositorio

---
+++ Parcial2-Ap2-Padilla/DAL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_Ap2_Padilla.DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
